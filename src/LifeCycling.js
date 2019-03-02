537451abf80f1ce409c7e0705ae4d9e4e202efc0
Xavi Toledano Martin <xavitoledanomartin@Xavis-MacBook-Pro.local>
2019-03-02 14:45:59 +0100

    LifeCycling, RefsComponents

---
+++ src/LifeCycling.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class LifeCycling extends React.Component {
+    constructor(){
+        super();
+        this.state = {val:0}
+        this.update = this.update.bind(this)
+    }
+    update(){
+        this.setState({val: this.state.val + 1})
+    }
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+    render(){
+        console.log('render');
+        return <button onClick={this.update}>{this.state.val}</button>
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    }
+}
+
+class Wrapper extends React.Component {
+    mount(){
+        ReactDOM.render(<LifeCycling/>, document.getElementById('a'))
+    }
+    unmount(){
+        ReactDOM.unmount.ComponentAtNode(document.getElementById('a'))
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="a"></div>
+            </div>
+        )
+    }
+}
+
+export default Wrapper;
--- src/RefsComponents.js
+++ src/RefsComponents.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class RefsComponents extends React.Component {
     constructor(){
